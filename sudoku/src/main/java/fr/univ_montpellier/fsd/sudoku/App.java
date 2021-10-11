eb604fefa00cc553640dec40da62d4428c15f999
Benjamin ADOLPHE <adolphe906@gmail.com>
2021-10-11 11:08:28 +0200

    Question 4

--- sudoku/src/main/java/fr/univ_montpellier/fsd/sudoku/App.java
+++ sudoku/src/main/java/fr/univ_montpellier/fsd/sudoku/App.java
@@ -1,13 +1,21 @@
 package fr.univ_montpellier.fsd.sudoku;
 
-import fr.univ_montpellier.fsd.sudoku.ppc.HelloWorld;
+import fr.univ_montpellier.fsd.sudoku.ppc.Sudoku;
 
 /**
  * Hello world!
+ *
  */
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        HelloWorld.main(args);
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Sudoku sudokuppc = new Sudoku();
+        System.out.println("Solution Sudoku proposé par PPC");
+        sudokuppc.solve();
+        System.out.println("-------------------------------------------------------");
+        System.out.println("Solution proposée par l'équipe");
+        fr.univ_montpellier.fsd.sudoku.imp.Sudoku sudokuimp = new fr.univ_montpellier.fsd.sudoku.imp.Sudoku(4);
+        sudokuimp.findSolution();
     }
 }
