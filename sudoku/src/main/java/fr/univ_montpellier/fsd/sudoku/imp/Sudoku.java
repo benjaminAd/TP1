cfb725f6d49044c071a2448eb5d1c4c9caf5d388
1visible <redx.6345@gmail.com>
2021-10-11 11:15:31 +0200

    Code solutionChecker

--- sudoku/src/main/java/fr/univ_montpellier/fsd/sudoku/imp/Sudoku.java
+++ sudoku/src/main/java/fr/univ_montpellier/fsd/sudoku/imp/Sudoku.java
@@ -23,14 +23,55 @@ public class Sudoku {
 	}
 
 	/*
+	 * [[x, x, x, x]
+	 * 	[x, x, x, x]
+	 * 	[x, x, x, x]
+	 * 	[x, x, x, x]]
+	 */
+
+	/*
 	 * check if this.grid is a correct sudoku solution.
 	 * 
 	 */
 
 	private boolean solutionChecker() {
-		// TODO
-		return false;
+		// Test de allDifferent sur chaque ligne
+		for (int i = 0; i < n; i++) {
+			int[] tab = new int[n];
 
+			for (int j = 0; j < n; j++)
+				tab[j] = grid[j][i];
+
+			if (!allDifferent(grid[i]) || !allDifferent(tab))
+				return false;
+		}
+
+		// Test de allDifferent sur les carrés
+		int squareSize = (int) Math.sqrt(n);
+		
+		for(int i = 0; i < n; i += squareSize) {
+			int[] tab = new int[n];
+			int counter = 0;
+			
+			for (int j = 0; j < squareSize; j++)
+				for (int k = 0; k < squareSize; k++) {
+					tab[counter] = grid[j+i][k+i];
+					counter++;
+				}
+			
+			if (!allDifferent(tab))
+				return false;
+		}
+
+		return true;
+	}
+
+	private boolean allDifferent(int... elements) {
+		for (int i = 0; i < elements.length - 1; i++)
+			for (int j = i + 1; j < elements.length; j++)
+				if (elements[i] == elements[j])
+					return false;
+		return true;
 	}
 
 	/*
