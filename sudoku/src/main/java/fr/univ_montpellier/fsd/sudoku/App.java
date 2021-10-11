4056300e8521338b12ab0a5589720d02bde39c5c
Daemoryn <daemoryn@gmail.com>
2021-10-11 11:06:13 +0200

    App main who call HelloWorld name

--- sudoku/src/main/java/fr/univ_montpellier/fsd/sudoku/App.java
+++ sudoku/src/main/java/fr/univ_montpellier/fsd/sudoku/App.java
@@ -1,13 +1,13 @@
 package fr.univ_montpellier.fsd.sudoku;
 
+import fr.univ_montpellier.fsd.sudoku.ppc.HelloWorld;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        HelloWorld.main(args);
     }
 }
